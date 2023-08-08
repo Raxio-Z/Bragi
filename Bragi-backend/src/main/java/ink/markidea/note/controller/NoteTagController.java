3f0405cb31d1a182bec350b9f8d163f87480d2ce
Louis Lee <42908892+CrackLewis@users.noreply.github.com>
2023-08-08 19:45:07 +0800

    update

--- Bragi-backend/src/main/java/ink/markidea/note/controller/NoteTagController.java
+++
@@ -1,8 +0,0 @@
-package ink.markidea.note.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-public class NoteTagController {
-}
